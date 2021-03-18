d4d2ae3ccd1e00cb98a872cbde0fbba7241015a0
Sourav Ganguly <souravganguly108@gmail.com>
2021-03-18 23:47:40 +0530

    javascript arrray and forEach loop example

--- index.js
+++ index.js
@@ -1,57 +1,14 @@
 
-var num1 = 10;
-var num2 = 3;
-console.log(`Num1 : ${num1}`);
-console.log(`Num2 : ${num2}`);
-console.log("Arithmetic Operator :");
-console.log(`Addition : ${num1+num2}`);
-console.log(`Substruction : ${num1-num2}`);
-console.log(`Multiplication : ${num1*num2}`);
-console.log(`Division : ${num1/num2}`);
-console.log(`Modulas : ${num1%num2}`);
 
-console.log(`Post Increment Opeator : ${num1++}`);
-console.log(`Post Decrement Opeator : ${num2--}`);
+let arr = ["Sourav", 'Raja', 'Ratul', 'Amartya', 'Rocket'];
 
-console.log(`Pre Increment Opeator : ${++num1}`);
-console.log(`Pre Decrement Opeator : ${--num2}`); 
+for (const item of arr) {
+    console.log(item);
+}
 
-num2--;
-console.log(num2);
+console.log(typeof(arr));
 
 
-console.log("Relational Operator :");
-
-var age1 = 22;
-var age2 = 17;
-
-console.log(age1==age2);
-console.log(age1!=age2);
-
-
-console.log(`age1 is greater then age2 : ${age1>age2}`);
-
-console.log("Logical operator : ");
-
-
-var name = 'Javascript';
-var isProgLang = true;
-
-console.log(name == "Javascript" && isProgLang );
-
-console.log(name=="javascript" || isProgLang);
-console.log(name=="javascript" || false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+arr.forEach(function (item, index){
+    console.log(index+". Value : "+ item);
+});

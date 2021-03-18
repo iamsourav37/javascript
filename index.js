c61ea95dc4f765e9ac01cc24ba0843a0cb63e4f6
Sourav Ganguly <souravganguly108@gmail.com>
2021-03-18 13:52:54 +0530

    onclick event

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+
+
+function sayHello(name){
+    console.log("Hello "+name);
+}
+
+function add(num1, num2){
+    return num1+num2;
+}
+
+
+sayHello("Sourav Ganguly");
+
+let fun = add;
+
+console.log(fun(3, 4));
+
+function btnClick(){
+    console.log("Button clicked");
+}

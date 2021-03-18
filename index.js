f10d783b9d312d64c390d33ff2e1e1689cfd474c
Sourav Ganguly <souravganguly108@gmail.com>
2021-03-18 16:05:27 +0530

    exercise time

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 
-
+console.log("Data Types : ");
 var myName = "Sourav Ganguly";
-
 console.log(typeof(myName));
 
 var myAge = 22;
-
 console.log(typeof(myAge));
 
 var flag = true;
-
 console.log(typeof(flag));
+console.log("------------");
+
+console.log("Exercise time : ");
+
+console.log("Number plus string :");
+console.log(10 + "10");
+
+console.log("Number minus string");
+console.log(9 - "4");
+console.log("This is a bug in Javascript");
+
+
+console.log("String plus String");
+console.log("Java" + "Script");
+
+console.log("String minius String : Not a number");
+console.log("Vinod" - "thapa");
+
+console.log(false - true);
+
+
+var a = null;
+console.log(typeof(a));
+
+var b = undefined;
+console.log(typeof(b));
+
+var x = NaN;
+console.log(typeof(x));
+console.log(isNaN(myName));

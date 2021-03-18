c0c82b95747e1c579c8ea201ec115ed5fe38f6d7
Sourav Ganguly <souravganguly108@gmail.com>
2021-03-18 17:08:10 +0530

    arithmetic operator

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 
-console.log(Number.NaN===Number.NaN);
-console.log(isNaN(NaN));
-console.log(isNaN(Number.isNaN));
+var num1 = 10;
+var num2 = 3;
+console.log(`Num1 : ${num1}`);
+console.log(`Num2 : ${num2}`);
+console.log("Arithmetic Operator :");
+console.log(`Addition : ${num1+num2}`);
+console.log(`Substruction : ${num1-num2}`);
+console.log(`Multiplication : ${num1*num2}`);
+console.log(`Division : ${num1/num2}`);
+console.log(`Modulas : ${num1%num2}`);
 
-console.log(Number.isNaN(NaN));
+console.log(`Post Increment Opeator : ${num1++}`);
+console.log(`Post Decrement Opeator : ${num2--}`);
+
+console.log(`Pre Increment Opeator : ${++num1}`);
+console.log(`Pre Decrement Opeator : ${--num2}`); 
+
+num2--;
+console.log(num2);

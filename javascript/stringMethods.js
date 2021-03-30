fde048b83c13dc76cb22bc3403a3fe9858c1c78b
iamsourav37 <souravganguly108@gmail.com>
2021-03-30 17:56:25 +0530

    string mehtods

--- javascript/stringMethods.js
+++ javascript/stringMethods.js
@@ -1,4 +1,31 @@
 // Javascript String mehtods
 
-let name = "Sourav Ganguly";
-console.log(name);
+let fav = "Python";
+
+console.log( fav.indexOf( "h" ) );
+
+console.log( fav.indexOf( "n0" ) );
+console.log( fav.search( "n0" ) );
+
+console.log( fav.slice( 1, 4 ) );
+console.log( fav.slice( 12, 23 ) );
+
+console.log( fav.substring( 1, 4 ) ); // substring() cannot accept negative indexes
+
+let greetings = "Hello, Sourav";
+
+// greetings = greetings.replace( /sourav/i, "World" ); // regular expression
+greetings = greetings.replace( "Sourav", "World" );
+// The replace() method does not change the string it is called on. It returns a new string
+console.log(greetings);
+
+greetings = greetings.toUpperCase();
+console.log( greetings );
+
+greetings = greetings.toLowerCase();
+console.log(greetings);
+
+
+let msg = "  hello world, how are you?   ";
+msg = msg.trim();
+console.log(msg);

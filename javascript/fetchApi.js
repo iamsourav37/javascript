45256c5e186635867588fe974f74d0550d579270
iamsourav37 <souravganguly108@gmail.com>
2021-03-31 10:57:50 +0530

    api example

--- javascript/fetchApi.js
+++ javascript/fetchApi.js
@@ -2,22 +2,26 @@ console.log("Fetch API example");
 
 const content = document.getElementById( "content" );
 const img = document.getElementById( "img" );
+const p_url = document.getElementById( "p_url" );
+const profile_url = document.getElementById( "profile_url" );
+
 
 function getData ()
 {
     // let url = "../data/myapi.txt";
-    let url = "https://api.github.com/users/10";
+    let url = "https://api.github.com/users";
     fetch( url ).then( ( response ) =>
     {
         return response.text();
     } ).then( ( data ) =>
     {
         let obj = JSON.parse( data );
-        console.log( obj['name']);
-        content.innerHTML = obj[ 'name' ];
-        console.log(obj[ "avatar_url" ] );
-        img.setAttribute( "src", obj[ "avatar_url" ] );
-        
+       
+        console.log( obj[ 0 ][ 'url' ] );
+        content.innerHTML = `Name : ${obj[ 0 ][ 'login' ]}`;
+        img.setAttribute( 'src', obj[ 0 ][ 'avatar_url' ] );
+        profile_url.setAttribute( 'href', `${ obj[ 0 ][ 'html_url' ] }` );
+        p_url.innerHTML = "Profile URL"
     } );
     
 }

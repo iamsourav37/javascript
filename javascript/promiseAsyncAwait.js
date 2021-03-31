b221ada23dc8dfaff07c80aed0645477ff19c4cf
iamsourav37 <souravganguly108@gmail.com>
2021-03-31 19:30:08 +0530

    async await example with github api

--- javascript/promiseAsyncAwait.js
+++ javascript/promiseAsyncAwait.js
@@ -1,18 +1,37 @@
-console.log( "Hello World" );
+// console.log("Hello World");
 
-new Promise( function ( resolve, reject )
-{
-    setTimeout( () =>
-    {
-        resolve( "this is resolved" );
-        // reject( "this is rejected" );
-    }, 4000 );
-} ).then( ( resolve ) =>
-{
-    console.log( "In then block" );
-    console.log( resolve );
-} ).catch( error =>
-{
-    console.log( "In catch block" );
-    console.log( error );
-} );
+// const demo = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     resolve("this is resolved");
+//     // reject( "this is rejected" );
+//   }, 4000);
+// });
+
+// demo
+//   .then((resolve) => {
+//     console.log("In then block");
+//     console.log(resolve);
+//   })
+//   .catch((error) => {
+//     console.log("In catch block");
+//     console.log(error);
+//   });
+
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     let data = "this data is comming from api call. (assume that)";
+//     // resolve( data );
+//     reject("error occured");
+//   }, 2000);
+// });
+
+const getData = async () => {
+  const response = await fetch("https://api.github.com/users/1");
+  const data = await response.json();
+  return data;
+};
+
+getData().then((data) => {
+  console.log("Inside then method");
+  console.log("data : ", data);
+});
